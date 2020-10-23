a3997c801657a8bda3735187f622b9262086e29f
Lazinelia <lazinelia@gmail.com>
2020-10-23 22:11:28 +0900

    [Style] フォーマッター警告を修正

--- extensions/image_log.py
+++ extensions/image_log.py
@@ -2,15 +2,16 @@ import datetime
 
 from discord.ext import commands
 
+
 class ImageLog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.bot:
             return
-            
+
         if message.guild.id != 408139758490091521:
             return
 
